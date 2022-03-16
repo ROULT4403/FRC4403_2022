c34b1cc38580c10584b77cc039bb310c2f650066
StarDust69 <82004679+StarDust69@users.noreply.github.com>
2022-03-15 22:42:03 -0500

    Shooter Gains Live Updates

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -23,6 +23,22 @@ public class Shooter extends SubsystemBase {
   private double shooterErrorTreshold = 35;
   private final int shooterSettleLoops = 25;
 
+  // Shooter gains
+  double newShooterConstant1;
+  double previousShooterConstant1 = 0;
+  double newShooterConstant2;
+  double previousShooterConstant2 = 0;
+  double newShooterConstant3;
+  double previousShooterConstant3 = 0;
+  double newShooterConstant4;
+  double previousShooterConstant4 = 0;
+  double newShooterConstant5;
+  double previousShooterConstant5 = 0;
+  double newShooterConstant6;
+  double previousShooterConstant6 = 0;
+  double newShooterConstant7;
+  double previousShooterConstant7 = 0;
+
   /** Creates a new Shooter. */
   public Shooter() {
     // Set inverted motor
@@ -38,6 +54,14 @@ public class Shooter extends SubsystemBase {
     shooterMotor.config_kI(0, ShooterConstants.shooterkI);
     shooterMotor.config_kD(0, ShooterConstants.shooterkD);
     shooterMotor.config_kF(0, ShooterConstants.shooterkF);
+
+    SmartDashboard.putNumber("ShooterConstant1", newShooterConstant1);
+    SmartDashboard.putNumber("ShooterConstant2", newShooterConstant2);
+    SmartDashboard.putNumber("ShooterConstant3", newShooterConstant3);
+    SmartDashboard.putNumber("ShooterConstant4", newShooterConstant4);
+    SmartDashboard.putNumber("ShooterConstant5", newShooterConstant5);
+    SmartDashboard.putNumber("ShooterConstant6", newShooterConstant6);
+    SmartDashboard.putNumber("ShooterConstant7", newShooterConstant7);
   }
   
   /** 
@@ -88,19 +112,19 @@ public class Shooter extends SubsystemBase {
    */
   public double getShooterTargetSpeed(){
     if (Robot.tD <= 120) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant1;
     } else if (Robot.tD <= 288) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + 5;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant2;
     } else if (Robot.tD <= 338) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant3;
     } else if (Robot.tD <= 388){
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant4;
     } else if (Robot.tD <= 438) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant5;
     } else if (Robot.tD <= 488) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant6;
     } else if (Robot.tD <= 538) {
-      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
+      return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458 + previousShooterConstant7;
     } else {
       return 0.001365 * Math.pow(Robot.tD, 2) + Robot.tD * 1.175 + 1458;
     }
@@ -114,5 +138,41 @@ public class Shooter extends SubsystemBase {
 
     SmartDashboard.putNumber("Distance", Robot.tD);
     SmartDashboard.putNumber("VisionYaw", Robot.tX);
+
+    // Tune hood values in Shuffleboard
+    newShooterConstant1 = SmartDashboard.getNumber("ShooterConstant1", 0.0);
+    if (newShooterConstant1 != previousShooterConstant1){
+      previousShooterConstant1 = newShooterConstant1;
+    }
+    
+    newShooterConstant2 = SmartDashboard.getNumber("ShooterConstant2", 0.0);
+    if (newShooterConstant2 != previousShooterConstant2){
+      previousShooterConstant2 = newShooterConstant2;
+    }
+
+    newShooterConstant3 = SmartDashboard.getNumber("ShooterConstant3", 0.0);
+    if (newShooterConstant3 != previousShooterConstant3){
+      previousShooterConstant3 = newShooterConstant3;
+    }
+
+    newShooterConstant4 = SmartDashboard.getNumber("ShooterConstant4", 0.0);
+    if (newShooterConstant4 != previousShooterConstant4){
+      previousShooterConstant4 = newShooterConstant4;
+    }
+
+    newShooterConstant5 = SmartDashboard.getNumber("ShooterConstant5", 0.0);
+    if (newShooterConstant5 != previousShooterConstant5){
+      previousShooterConstant5 = newShooterConstant5;
+    }
+
+    newShooterConstant6 = SmartDashboard.getNumber("ShooterConstant6", 0.0);
+    if (newShooterConstant6 != previousShooterConstant6){
+      previousShooterConstant6 = newShooterConstant6;
+    }
+
+    newShooterConstant7 = SmartDashboard.getNumber("ShooterConstant7", 0.0);
+    if (newShooterConstant7 != previousShooterConstant7){
+      previousShooterConstant7 = newShooterConstant7;
+    }
   }
 }
